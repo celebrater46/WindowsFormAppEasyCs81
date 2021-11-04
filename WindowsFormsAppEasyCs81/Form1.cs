37bb7afa9f5b3a58a8dc949f46094607a2fe2b9b
hideru <jota.16.stigma.world@gmail.com>
2021-11-04 14:24:02 +0900

    Form1 appeared but image is too small.

---
+++ WindowsFormsAppEasyCs81/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEasyCs81
+{
+    public partial class Form1 : Form
+    {
+        private Image img;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "画像テストやで！";
+            this.Width = 250;
+            this.Height = 200;
+
+            img = Image.FromFile("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs81\\WindowsFormsAppEasyCs81\\img\\sunsymbol3.png");
+
+            this.Click += new EventHandler(ClickForm);
+            this.Paint += new PaintEventHandler(PaintForm);
+        }
+
+        public void ClickForm(Object sender, EventArgs e)
+        {
+            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            this.Invalidate();
+        }
+
+        public void PaintForm(Object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(img, 0, 0);
+        }
+    }
+}
